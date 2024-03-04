59d1e891747edf87bf8df5c05ffad29359c9e448
Rafhaan Ahmed Fayaz (FD) <rafhaan_2425@dhiraagu.com.mv>
2024-03-04 14:39:08 +0500

    Eslint for cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     browser: true,
     es6: true,
     "jest/globals": true,
+    "cypress/globals": true,
     node: true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
