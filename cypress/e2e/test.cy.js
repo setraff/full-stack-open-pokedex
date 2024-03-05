17e18f7bda86c360d2a659ad6817ab63ae8b8226
ra2-fayaz <rafhaan2.fayaz@live.uwe.ac.uk>
2024-03-05 23:01:36 +0500

    Health check deployed

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -11,4 +11,7 @@ describe("Pokedex", function () {
     cy.contains("ivysaur").click();
     cy.contains("chlorophyll");
   });
+  it("Health check passes", function () {
+    cy.contains("ok");
+  });
 });
