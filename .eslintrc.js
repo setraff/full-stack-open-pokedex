d34f8d6118400a2d70a95f3914f06a1b4b943b6f
ra2-fayaz <rafhaan2.fayaz@live.uwe.ac.uk>
2024-03-05 23:09:32 +0500

    Fuck eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,5 +28,6 @@ module.exports = {
     "arrow-spacing": ["error", { before: true, after: true }],
     "no-console": "error",
     "react/prop-types": 0,
+    "no-constant-condition": 0,
   },
 };
