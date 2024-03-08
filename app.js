7d6e4b1acbb3c1e85f135f61ad81aa4bb58abdb7
ra2-fayaz <rafhaan2.fayaz@live.uwe.ac.uk>
2024-03-08 16:16:34 +0500

    Hmm

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-// get the port from environment variable Helo
+// get the port from environment variable Helo what the fuck bro
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
