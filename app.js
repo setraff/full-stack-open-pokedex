a0d69eed412934a325138fe363c809b828cc85c7
ra2-fayaz <rafhaan2.fayaz@live.uwe.ac.uk>
2024-03-13 20:18:52 +0500

    HEalth check reduced

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 
 app.get("/health", (req, res) => {
-  res.send("HEALTH OK");
+  // res.send("HEALTH OK");
+  res.sendStatus(500);
 });
 
 app.listen(PORT, () => {
